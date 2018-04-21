750ae467d36f771ffbe14ce606af7b2ab5f95fd6
Jerry Liu <zhengyu.liu@duke.edu>
2018-04-21 14:20:41 -0400

    initial firebase setup

--- app/build/generated/source/r/debug/android/support/compat/R.java
+++ app/build/generated/source/r/debug/android/support/compat/R.java
@@ -83,12 +83,12 @@ public final class R {
         public static final int notification_background = 0x7f07004e;
         public static final int notification_main_column = 0x7f07004f;
         public static final int notification_main_column_container = 0x7f070050;
-        public static final int right_icon = 0x7f070059;
-        public static final int right_side = 0x7f07005a;
-        public static final int text = 0x7f07007b;
-        public static final int text2 = 0x7f07007c;
-        public static final int time = 0x7f07007f;
-        public static final int title = 0x7f070080;
+        public static final int right_icon = 0x7f07005a;
+        public static final int right_side = 0x7f07005b;
+        public static final int text = 0x7f07007c;
+        public static final int text2 = 0x7f07007d;
+        public static final int time = 0x7f070080;
+        public static final int title = 0x7f070081;
     }
     public static final class integer {
         public static final int status_bar_notification_info_maxnum = 0x7f080004;
