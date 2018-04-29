6bfe473ea22ec389f6ef3044918c34fc334647c3
Jerry Liu <zhengyu.liu@duke.edu>
2018-04-29 12:10:20 -0400

    figured out how to convert images to string64 and back

--- app/src/main/java/edu/duke/compsci290/fpx/MainActivity.java
+++ app/src/main/java/edu/duke/compsci290/fpx/MainActivity.java
@@ -2,13 +2,19 @@ package edu.duke.compsci290.fpx;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.renderscript.Sampler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.ChildEventListener;
@@ -18,6 +24,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
@@ -107,8 +117,58 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         dbref.addListenerForSingleValueEvent(postListener);
+        //Figuring out picture stuff
+        AssetManager assetManager = getAssets();
+        for(String s: assetManager.getLocales()) {
+            Log.d("Lol", s
+            );
+        }
+        try {
+                InputStream input = assetManager.open("willye.png");
+            byte[] bytes;
+            byte[] buffer = new byte[8192];
+            int bytesRead;
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+                while ((bytesRead = input.read(buffer)) != -1) {
+                    output.write(buffer, 0, bytesRead);
+                }
+            bytes = output.toByteArray();
+            String encodedString = Base64.encodeToString(bytes, Base64.DEFAULT);
+            FirebaseUtilities.updateOrCreateUser(new User("mysteryman", false,"2021", "CS/STATS", "Jerry Liu", "8586632671", encodedString));
+            Log.d("lol", "hopefully soemtthing good happpened");
+        } catch (IOException e) {
+            Log.d("lol", "fuck");
+                e.printStackTrace();
+            }
+        DatabaseReference dbref2 = FirebaseDatabase.getInstance().getReference().child("users").child("mysteryman");
+        ValueEventListener postListener2 = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d("FIREBASE%%%%%%%%%%%", (String)dataSnapshot.child("mName").getValue(true));
+                User user = dataSnapshot.getValue(User.class);
+                Log.d("FIREBASE%%%%%%%%%%%", user.getmNetID() + user.getmName() + user.getmIsGiving());
+                //converting picture from string64 to bitmap
+                byte[] imageAsBytes = Base64.decode(user.getmPhoto().getBytes(), Base64.DEFAULT);
+                Bitmap bm = BitmapFactory.decodeByteArray(imageAsBytes, 0, imageAsBytes.length);
+
+                ImageView img= (ImageView) findViewById(R.id.imageView);
+                img.setImageBitmap(bm);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w("firebase fucked", "loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+        };
+        dbref2.addListenerForSingleValueEvent(postListener2);
+
 
     }
+
+
+
 
 
 }
