7cb94c58262e974dcf7f297409b35ac279f5967c
Serena Liu (sl362) <sl362@duke.edu>
2018-04-29 23:35:05 -0400

    created tests

---
+++ app/src/test/java/edu/duke/compsci290/fpx/ProfileTest.java
@@ -0,0 +1,8 @@
+package edu.duke.compsci290.fpx;
+
+/**
+ * Created by Serena on 4/29/18.
+ */
+
+public class ProfileTest {
+}
---
+++ app/src/test/java/edu/duke/compsci290/fpx/TransactionTest.java
@@ -0,0 +1,19 @@
+package edu.duke.compsci290.fpx;
+import org.junit.Test;
+import java.util.regex.Pattern;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Serena on 4/29/18.
+ */
+
+public class TransactionTest {
+
+
+        @Test
+        public void Transactionblah() {
+
+        }
+
+}
